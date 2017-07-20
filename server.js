44e17f462b2312f2cbf67dac09e8f6617a8dbe08
TheJondar <Johnsantana87@gmail.com>
2017-07-19 20:24:24 -0400

    initial commit and push

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+const passport = require('passport');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const env = require('dotenv').load();
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Passport with Sequelize');
+});
+
+app.listen(3306, (err) => {
+  if (!err) {
+    console.log('it lives!');
+  } else console.log(err);
+});
+
+// For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
