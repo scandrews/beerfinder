56b52c6da2645a7829beaa0f2236ab8b0229ba48
TheJondar <Johnsantana87@gmail.com>
2017-07-20 20:55:16 -0400

    [from now] 2017/07/20 20:55:16
    diff --git a/app/controllers/authcontroller.js b/app/controllers/authcontroller.js
    index ca8caec..8e9f5b0 100644
    --- a/app/controllers/authcontroller.js
    +++ b/app/controllers/authcontroller.js
    @@ -2,3 +2,7 @@ var exports = module.exports = {};
     exports.signup = (req, res) => {
       res.render('signup');
     };
    +
    +exports.signin = (req, res) => {
    +  res.render('signin');
    +}
    \ No newline at end of file
    diff --git a/app/routes/auth.js b/app/routes/auth.js
    index 6f1e0f6..a3c35ce 100644
    --- a/app/routes/auth.js
    +++ b/app/routes/auth.js
    @@ -3,3 +3,5 @@ const authController = require('../controllers/authcontroller.js');
     module.exports = (app) => {
       app.get('/signup', authController.signup);
     };
    +
    +app.get('/signin', authController.signin);
    \ No newline at end of file
    diff --git a/app/views/signin.hbs b/app/views/signin.hbs
    new file mode 100644
    index 0000000..104edd0
    --- /dev/null
    +++ b/app/views/signin.hbs
    @@ -0,0 +1,19 @@
    +<!DOCTYPE html>
    +<html>
    +
    +<head>
    +    <title>Sign in</title>
    +</head>
    +
    +<body>
    +    <form id="signin" name="signin" method="post" action="signin">
    +        <label for="email">Email Address</label>
    +        <input class="text" name="email" type="text" />
    +        <label for="password">Password</label>
    +        <input name="password" type="password" />
    +        <input class="btn" type="submit" value="Sign In" />
    +    </form>
    +
    +</body>
    +
    +</html>
    \ No newline at end of file

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -2,3 +2,7 @@ var exports = module.exports = {};
 exports.signup = (req, res) => {
   res.render('signup');
 };
+
+exports.signin = (req, res) => {
+  res.render('signin');
+}
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -3,3 +3,5 @@ const authController = require('../controllers/authcontroller.js');
 module.exports = (app) => {
   app.get('/signup', authController.signup);
 };
+
+app.get('/signin', authController.signin);
