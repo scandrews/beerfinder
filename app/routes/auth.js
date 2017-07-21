151398c5e5585ca25d34da2f8318f5ae72b07ba4
TheJondar <Johnsantana87@gmail.com>
2017-07-20 20:57:03 -0400

    [from now] 2017/07/20 20:57:03
    diff --git a/app/routes/auth.js b/app/routes/auth.js
    index a3c35ce..9835cdc 100644
    --- a/app/routes/auth.js
    +++ b/app/routes/auth.js
    @@ -2,6 +2,6 @@ const authController = require('../controllers/authcontroller.js');
    
     module.exports = (app) => {
       app.get('/signup', authController.signup);
    -};
    
    -app.get('/signin', authController.signin);
    \ No newline at end of file
    +  app.get('/signin', authController.signin);
    +};

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -2,6 +2,6 @@ const authController = require('../controllers/authcontroller.js');
 
 module.exports = (app) => {
   app.get('/signup', authController.signup);
-};
 
-app.get('/signin', authController.signin);
+  app.get('/signin', authController.signin);
+};
