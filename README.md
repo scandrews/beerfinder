2506ae95ae2853dd2321c869de51287d4fe9f26b
Steve Andrews <scandrews2@gmail.com>
2017-07-24 20:08:19 -0400

    minor changes steve

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # beerfinder
 Project 2 - The beer finder app
+
+An app to help you find new beers that you will enjoy
+
+## Ovverview
+
+This is a web application built on node, using handlebars and Bootstrap for the front end.  The app will require a login and track user's preferences and history
+
+## Homepage
+
+
+
+##Functionality
+
+The app is a node js server and controllers, with a custom built ORM using express, assessing a SQL database.  The front end is using handlebars and Bootstrap, also Passport is used for user login and authentication.  Passport uses Sequelize to assess the SQL database.
+
+Cloneing the repo
+
+To clone the app to your machine:
+
+	1.	Make sure SQL is running on your machine
+	2.	The app uses a database called beer_db
+	3.	Install required node pacakges with NPM install
+	4.	To start the server - node server.js
+	5.	The front end access is through localhost3000
+
+##Screenshots
+
+
+
+
+
+## Technologies Used
+
+npm express
+npm express-handlebars
--- config/algorithms.js
+++ config/algorithms.js
@@ -1,73 +1,75 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
-	//your username
-	user: "root",
-	//your password
-	password:"",
-	database: "beer_db"
+    host: "localhost",
+    port: 3306,
+    //your username
+    user: "root",
+    //your password
+    password:"",
+    database: "beer_db"
 
 });
 connection.connect(function start(err){
-	if (err)  throw err;
+    if (err)  throw err;
 
 
 //basic test to log all the beer names
 var listAll = function(){
-	var beerList = connection.query('SELECT * FROM beerTbl', function(err, result){
-		if (err) throw err;
-			for (var i = result.length - 1; i >= 0; i--) {
-				console.log(result[i].name + '\n');
-		}
-	});//test works 7-19 TG
+    var beerList = connection.query('SELECT * FROM beerTbl', function(err, result){
+        if (err) throw err;
+            for (var i = result.length - 1; i >= 0; i--) {
+                console.log(result[i].name + '\n');
+                var allBeers = {allBeers:result[i].name};
+                return(allBeers);
+        }
+    });//test works 7-19 TG
 }
 //list the beers out by type
 var listByType = function(){
-	var styleSearch = "IPA";
-	var beerList = connection.query('SELECT * FROM beerTbl WHERE ?', [{style: styleSearch}], function(err, result){
-		if (err) throw err; 
-			for (var i = 0; i < result.length; i++) {	
-				console.log(result[i].name + '_____' + result[i].style);
+    var styleSearch = "IPA";
+    var beerList = connection.query('SELECT * FROM beerTbl WHERE ?', [{style: styleSearch}], function(err, result){
+        if (err) throw err; 
+            for (var i = 0; i < result.length; i++) {    
+                console.log(result[i].name + '_____' + result[i].style);
 
-			}
-	});//test works 7-19 TG
+            }
+    });//test works 7-19 TG
 }
 //search the db by name
 var searchDB = function(){
-	var prompt = "Heady Topper";		 //this will store the users inputed search
-		var findMeBeers = function(){ //this is a general search function for the db - search by name
-			var beerList = connection.query('SELECT * FROM beerTbl WHERE ?', [{name:prompt}],
-			  function(err, result){
-				if (err) throw err;	
-				console.log("prompt:  " + prompt);
-					if (result.length == 0) {
-						console.log("nothing found");
-					}else{
-							for (var i = 0; i < result.length; i++) {
-								console.log(result[i].name);
-							}
-						}
-			});
-		}
-	findMeBeers();
+    var prompt = "Heady Topper";         //this will store the users inputed search
+        var findMeBeers = function(){ //this is a general search function for the db - search by name
+            var beerList = connection.query('SELECT * FROM beerTbl WHERE ?', [{name:prompt}],
+              function(err, result){
+                if (err) throw err;    
+                console.log("prompt:  " + prompt);
+                    if (result.length == 0) {
+                        console.log("nothing found");
+                    }else{
+                            for (var i = 0; i < result.length; i++) {
+                                console.log(result[i].name);
+                            }
+                        }
+            });
+        }
+    findMeBeers();
 }
 //match a chosen beer to similar beers in the db - STILL IN PROGRESS
 var matchBeer = function(){
-	var chosenBeerId = 1;
-	//pull beer 1 from db and store in var
-	var selectedBeer = connection.query('SELECT * FROM beerTbl WHERE ?', [{id:chosenBeerId}],
- 	function(err, result){
-		if (err) throw err; 
-		for (var i = 0; i < result.length; i++) {
-			console.log("matching beers to: " + result[0].name);
-			//grabbing selected beer identifiers to match with
-			var searchIbu = result[0].hoppieness;
-			var searchColor = result[0].color;
-			var searchStyle = result[0].style;
-			// var smell = result[0].smell; //we need a much larger db for this to work - removed for now
-		}
+    var chosenBeerId = 1;
+    //pull beer 1 from db and store in var
+    var selectedBeer = connection.query('SELECT * FROM beerTbl WHERE ?', [{id:chosenBeerId}],
+     function(err, result){
+        if (err) throw err; 
+        for (var i = 0; i < result.length; i++) {
+            console.log("matching beers to: " + result[0].name);
+            //grabbing selected beer identifiers to match with
+            var searchIbu = result[0].hoppieness;
+            var searchColor = result[0].color;
+            var searchStyle = result[0].style;
+            // var smell = result[0].smell; //we need a much larger db for this to work - removed for now
+        }
 // SELECT * FROM MyTable WHERE (Column1 LIKE '%keyword1%' OR Column2 LIKE 
 // '%keyword1%') AND (Column1 LIKE '%keyword2%' OR Column2 LIKE '%keyword2%');
 
@@ -76,38 +78,43 @@ ibuLimitLow = searchIbu - 25;
 colorLimitHigh = searchColor + 2;
 colorLimitLow = searchColor - 2;
 
-			//based on stats from beer 1 identify matching beers	 searchIbu -25,'AND', searchIbu + 25,
-	var matchedBeers = connection.query('SELECT * FROM beerTbl WHERE hoppieness BETWEEN ' + ibuLimitLow + ' AND ' + ibuLimitHigh + ' AND ' + colorLimitLow + ' AND ' + colorLimitHigh,  
-		function(err, result){
-			if(err) throw err;
-			for (var i = 0; i < result.length; i++) {
-				console.log("your beer matches with: " + result[i].name);
-			}
-		});	
-	});	
+            //based on stats from beer 1 identify matching beers     searchIbu -25,'AND', searchIbu + 25,
+    var matchedBeers = connection.query('SELECT * FROM beerTbl WHERE hoppieness BETWEEN ' + ibuLimitLow + ' AND ' + ibuLimitHigh + ' AND ' + colorLimitLow + ' AND ' + colorLimitHigh,  
+        function(err, result){
+            if(err) throw err;
+            for (var i = 0; i < result.length; i++) {
+                console.log("your beer matches with: " + result[i].name);
+                var returnMatched = {matched: result[i].name};
+                return(returnMatched); 
+            }
+        });    
+    });    
 }
 
 
 //find a random beer of the day
-var beerOfTheDay = function(){
-	var beerOfTheDayID = Math.floor((Math.random() * 20) + 1);
-	console.log(beerOfTheDayID);
-		find = connection.query('SELECT * FROM beerTbl WHERE ?', [{id:beerOfTheDayID}], 
-			function(err, result){
-				if (err) throw err;
-				console.log("The Beer of the Day is: " + result[0].name);
-			});
+var getBeerOfTheDay = function(){
+    var beerOfTheDayID = Math.floor((Math.random() * 20) + 1);
+    console.log(beerOfTheDayID);
+        find = connection.query('SELECT * FROM beerTbl WHERE ?', [{id:beerOfTheDayID}], 
+            function(err, result){
+                if (err) throw err;
+                console.log("The Beer of the Day is: " + result[0].name);
+                // res.json(result[0].name);
+                var beerOfTheDay = {beer: result[0].name};
+                return(beerOfTheDay);
+            });
 }
-
-
-
-//my sql .escape makes usre the variable you're using is ok to use with SQL
-// beerOfTheDay(); 	//	7/22 working TG
-// matchBeer();		//	7/22 working TG
-// searchDB();		//	7-22 working TG
-// listAll();		//	7-22 working TG
-// listByType();	// 	7-22 working TG
+     
+// searchDB();        //    7-22 working TG
+// listAll();        //    7-22 working TG
+// listByType();    //     7-22 working TG
 
 });//end of connection
 
-
+var algorithms = {
+    getBeerOfTheDay: getBeerOfTheDay,
+    matchBeer: matchBeer,
+    listAll: listAll    
+}
+module.exports = algorithms;
