95dacea97eaf62c8c16759506386d81b583f9d62
Steve Andrews <scandrews2@gmail.com>
2020-03-29 16:08:44 -0400

    changed process variable implementation for Heroku

--- models/index.js
+++ models/index.js
@@ -5,14 +5,23 @@ const Sequelize = require('sequelize');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
 const db = {};
+
+if (process.env.JAWSDB_URL){
+	var sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else
+if (config.use_env_variable) {
+	var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+	var sequelize = new Sequelize(config.database, config.username, config.password, config);
+};
+
 
 fs
   .readdirSync(__dirname)
   .filter(file => (file.indexOf('.') !== 0) && (file !== 'index.js'))
   .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
+    const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
